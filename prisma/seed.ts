2f7bae4c8bc0b68b43d707d4d16a0827787258bf
User <user@MacBook-Air-User.local>
2026-04-12 15:01:57 +0200

    fix: ensure PrismaClient in seed script has access to DATABASE_URL

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,14 @@
+import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
 async function main() {
   const hashedPassword = await bcrypt.hash("password123", 12);
